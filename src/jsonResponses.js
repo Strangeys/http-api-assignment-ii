bd97e32d014a7f8eb8293cbf7e0f0bc55883cd77
Nick-Malone <>
2022-09-25 23:16:17 -0400

    Finish HW

---
+++ src/jsonResponses.js
@@ -0,0 +1,75 @@
+const users = {};
+
+const headers = {
+    'Content-Type': 'application/json',
+};
+
+const respondJSON = (request, response, status, object) => {
+    response.writeHead(status, headers);
+    response.write(JSON.stringify(object));
+    response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+    response.writeHead(status, headers);
+    response.end();
+};
+
+const getUsers = (request, response) => {
+    const responseJSON = {
+        users,
+    };
+
+    if (request.method === 'HEAD') {
+        return respondJSONMeta(request, response, 200);
+    }
+
+    return respondJSON(request, response, 200, responseJSON);
+};
+
+const addUser = (request, response, body) => {
+    const responseJSON = {
+        message: 'Name and age are both required.'
+    };
+
+    // throw error is params missing
+    if(!body.name || !body.age) {
+        respondJSON.id = 'missingParams';
+        return respondJSON(request, response, 400, responseJSON);
+    }
+    
+    let responseCode = 201;
+
+    if (users[body.name]) {
+        responseCode = 204;
+    } else {
+        users[body.name] = {};
+    }
+
+    users[body.name].name = body.name;
+    users[body.name].age = body.age;
+
+    if(responseCode === 201){
+        responseJSON.message = 'Created Successfully';
+        return respondJSON(request, response, responseCode, responseJSON);
+    }
+
+    // return 204
+    responseJSON.message = 'Updated content';
+    return respondJSON(request, response, responseCode, responseJSON);
+};
+
+const notReal = (request, response) => {
+    const responseJSON = {
+        message: 'The page you are looking for was not found.',
+        id: 'notFound',
+    };
+
+    respondJSON(request, response, 404, responseJSON);
+};
+
+module.exports = {
+    getUsers,
+    addUser,
+    notReal,
+};
