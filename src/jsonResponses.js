0ecc591fc3b1a088e0b342f5ba0e234855d7f870
Nick-Malone <>
2022-09-25 23:28:03 -0400

    Actually install ESLint

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,75 +1,75 @@
 const users = {};
 
 const headers = {
-    'Content-Type': 'application/json',
+  'Content-Type': 'application/json',
 };
 
 const respondJSON = (request, response, status, object) => {
-    response.writeHead(status, headers);
-    response.write(JSON.stringify(object));
-    response.end();
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(object));
+  response.end();
 };
 
 const respondJSONMeta = (request, response, status) => {
-    response.writeHead(status, headers);
-    response.end();
+  response.writeHead(status, headers);
+  response.end();
 };
 
 const getUsers = (request, response) => {
-    const responseJSON = {
-        users,
-    };
+  const responseJSON = {
+    users,
+  };
 
-    if (request.method === 'HEAD') {
-        return respondJSONMeta(request, response, 200);
-    }
+  if (request.method === 'HEAD') {
+    return respondJSONMeta(request, response, 200);
+  }
 
-    return respondJSON(request, response, 200, responseJSON);
+  return respondJSON(request, response, 200, responseJSON);
 };
 
 const addUser = (request, response, body) => {
-    const responseJSON = {
-        message: 'Name and age are both required.'
-    };
+  const responseJSON = {
+    message: 'Name and age are both required.',
+  };
 
-    // throw error is params missing
-    if(!body.name || !body.age) {
-        respondJSON.id = 'missingParams';
-        return respondJSON(request, response, 400, responseJSON);
-    }
-    
-    let responseCode = 201;
+  // throw error is params missing
+  if (!body.name || !body.age) {
+    respondJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
 
-    if (users[body.name]) {
-        responseCode = 204;
-    } else {
-        users[body.name] = {};
-    }
+  let responseCode = 201;
 
-    users[body.name].name = body.name;
-    users[body.name].age = body.age;
+  if (users[body.name]) {
+    responseCode = 204;
+  } else {
+    users[body.name] = {};
+  }
 
-    if(responseCode === 201){
-        responseJSON.message = 'Created Successfully';
-        return respondJSON(request, response, responseCode, responseJSON);
-    }
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
 
-    // return 204
-    responseJSON.message = 'Updated content';
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
     return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  // return 204
+  responseJSON.message = 'Updated content';
+  return respondJSON(request, response, responseCode, responseJSON);
 };
 
 const notReal = (request, response) => {
-    const responseJSON = {
-        message: 'The page you are looking for was not found.',
-        id: 'notFound',
-    };
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
 
-    respondJSON(request, response, 404, responseJSON);
+  respondJSON(request, response, 404, responseJSON);
 };
 
 module.exports = {
-    getUsers,
-    addUser,
-    notReal,
+  getUsers,
+  addUser,
+  notReal,
 };
